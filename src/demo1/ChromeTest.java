44981c524c3e1af4cce60f79545832a61ce55e9d
Vaibhav Narayan Nirphal <vn22275@corp.ojas-it.com>
2022-08-29 16:53:48 +0530

    commit name changes

---
+++ src/demo1/ChromeTest.java
@@ -0,0 +1,11 @@
+package demo1;
+
+import org.openqa.selenium.WebDriver;
+
+public class ChromeTest {
+public static void main(String[] args) {
+	System.setProperty("webdriver.chrome.driver", "C:\\Users\\vn22275\\eclipse-workspace\\Dremo\\Drivers\\chromedriver.exe");
+	WebDriver driver = new ChromeDriver();
+	
+}
+}
